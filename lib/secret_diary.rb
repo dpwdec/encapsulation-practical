efa9452d7493161c7e77f9216afda6de3c3ce8e3
dpwdec <declankolakowski@gmail.com>
2020-04-19 11:44:45 +0100

    Refactored lock functionality into Lock class and removed tests and associated code from diary class

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,29 +1,20 @@
 class SecretDiary
 
-  attr_reader :locked
+  attr_reader :locker
 
   def initialize
     @locked = true
     @entries = []
-  end
-
-  def lock
-    raise 'already locked' if @locked
-    @locked = true
-  end
-
-  def unlock
-    raise 'already unlocked' if !@locked
-    @locked = false
+    @locker = Lock.new
   end
 
   def add_entry(entry)
-    raise 'diary is locked' if @locked
+    raise 'diary is locked' if locker.locked
     @entries << entry
   end
 
   def get_entries
-    raise 'diary is locked' if @locked
+    raise 'diary is locked' if locker.locked
     return @entries
   end
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -2,46 +2,8 @@ require 'secret_diary'
 
 describe SecretDiary do
 
-  it { is_expected.to respond_to(:locked) }
-  it { is_expected.to respond_to(:lock) }
-  it { is_expected.to respond_to(:unlock) }
-
   it { is_expected.to respond_to(:add_entry).with(1).argument }
   it { is_expected.to respond_to(:get_entries) }
-
-  it "is locked" do
-    expect(subject.locked).to be true
-  end
-
-  describe "#lock" do
-    context "it is locked" do
-      it "raises an error 'already locked'" do
-        expect { subject.lock }.to raise_error('already locked')
-      end
-
-      context "is is unlocked" do
-        it "changes @locked to be true" do
-          subject.unlock
-          expect { subject.lock }.to change(subject, :locked).to be true
-        end
-      end
-    end
-  end
-
-  describe "#unlock" do
-    context "it is locked" do
-      it "changes @locked to be false" do
-        expect { subject.unlock }.to change(subject, :locked).to be false
-      end
-    end
-
-    context "it is unlocked" do
-      it "raises an error 'already unlocked'" do
-        subject.unlock
-        expect { subject.unlock }.to raise_error('already unlocked')
-      end
-    end
-  end
 
   describe "#add_entry" do
     context "it is locked" do
@@ -52,7 +14,7 @@ describe SecretDiary do
 
     context "it is unlocked" do
       it "adds an entry to diary" do
-        subject.unlock
+        subject.locker.unlock
         expect { subject.add_entry('Dear diary') }.to change(subject, :get_entries).to include('Dear diary')
       end
     end
@@ -67,12 +29,12 @@ describe SecretDiary do
 
     context "it is unlocked" do
       it "returns an array" do
-        subject.unlock
+        subject.locker.unlock
         expect(subject.get_entries).to be_a_kind_of(Array)
       end
 
       it "returns a list of diary entries" do
-        subject.unlock
+        subject.locker.unlock
         entry_1 = 'My first entry.'
         entry_2 = 'My day in the woods.'
         subject.add_entry(entry_1)
